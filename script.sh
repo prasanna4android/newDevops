9b3ff17f495e247cb457804a0d45b4d2f877f271
prasanna4android <prasanna4android@gmail.com>
2019-04-14 12:38:51 +0530

    Artifacts Archive

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "[INFO] running script available from Github Via Jenkins"
- echo"[INFO] Running from github again 12:36"
- 
-  echo"[INFO] Artifacts Archive"
+echo "[INFO] Running from github again 12:36"
+echo "[INFO] Artifacts Archive"
 
