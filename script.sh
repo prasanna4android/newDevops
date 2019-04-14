3c62bb49e987dd46e101c5b04f22296c887bb8f6
prasanna4android <prasanna4android@gmail.com>
2019-04-14 13:07:34 +0530

    demo for triggering build other project

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 echo "[INFO] running script available from Github Via Jenkins"
 echo "[INFO] Running from github again 12:36"
 echo "[INFO] Artifacts Archive Updated "
-echo "[INFO] demo for triggering build other project  "
+echo "[INFO] demo for triggering build other project  11"
 
